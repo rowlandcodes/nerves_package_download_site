c81a0238d79170623f27d1533bbc97b0097da08d
Rowland Carlson <rowland@stillclever.com>
2022-03-04 18:05:45 -1000

    Adds a public s3 bucket to store nerves packages

---
+++ s3/main.tf
@@ -0,0 +1,8 @@
+resource "aws_s3_bucket" "package_download_bucket" {
+  bucket = var.bucket_name
+}
+
+resource "aws_s3_bucket_acl" "package_download_bucket" {
+   bucket = aws_s3_bucket.package_download_bucket.id
+   acl = "public"
+}
